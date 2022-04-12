5c2459d13d2cee418b6c1e140fa57d1d271ca527
samakunchan <lesdieux974>
2022-04-13 00:28:33 +0200

    presque

---
+++ lib/core/models/note_response_model.dart
@@ -0,0 +1,32 @@
+
+import 'note_model.dart';
+
+class NoteResponseModel {
+  List results;
+  bool error;
+  String message;
+
+   NoteResponseModel({
+    required this.results,
+    required this.error,
+    required this.message
+  });
+
+  factory NoteResponseModel.fromJson(Map<String, dynamic> json) {
+    return NoteResponseModel(
+      results: (json['results'] as List).map((item) => NoteModel(
+          id: item['id'],
+          title: item['title'],
+          description: item['description'],
+          createdAt: DateTime.tryParse(item['createdAt']),
+          updatedAt: DateTime.tryParse(item['updatedAt']),
+      )).toList(),
+      error: json['error'],
+      message: json['message'],
+    );
+  }
+
+  factory NoteResponseModel.withError(Map<String, dynamic> json) {
+    return NoteResponseModel(results: [], error: true, message: json['message']);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:rx_dart_notes/pages/home_page.dart';
+import 'core/services/notes_service.dart';
+
+void setUpLocator(){
+  GetIt.I.registerLazySingleton(() => NoteService());
+}
 
 void main() {
+  setUpLocator();
   runApp(const MyApp());
 }
 
